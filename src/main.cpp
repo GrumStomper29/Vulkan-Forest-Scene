74f649f5d74c74970f0703f87bc6ce79a68b7d45
Lucas <lucasburns725@gmail.com>
2023-05-18 16:50:16 -0400

    Window Test

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,33 @@
+#define GLFW_INCLUDE_NONE
+#include "glfw/glfw3.h"
+
+#include <cstdlib>
 #include <iostream>
 
 int main()
 {
-	std::cout << "Hello world!\n";
-	std::cin.get();
+	if (!glfwInit())
+	{
+		std::cerr << "error: failed to initialize GLFW.\n";
+		std::abort();
+	}
+
+	GLFWwindow* window{ glfwCreateWindow(640, 480, "Vulkan Forest Scene", nullptr, nullptr) };
+
+	if (!window)
+	{
+		std::cerr << "error: failed to create window.\n";
+		std::abort();
+	}
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+
+	glfwTerminate();
 
 	return 0;
 }
